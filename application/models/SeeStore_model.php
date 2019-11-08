9cc1244ad39bd33953aa9e4836aade52fa38ef7d
K00215698 <K00215698@ITSB-05.student.litdom.lit.ie>
2019-11-08 10:36:24 +0000

    General Fixes

--- application/models/SeeStore_model.php
+++ application/models/SeeStore_model.php
@@ -1,15 +1,37 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SeeStore_model extends CI_Controller {
+class SeeStore_model extends CI_Controller
+{
 
-	function get_store_categories() {
-		
-		$categories = $this->db->query('CALL getCategories()');
-		
-		return $categories; 
-	}
+    function get_store_categories()
+    {
+        $categories = $this->db->query('CALL getCategories()');
+        return $categories;
+    }
+
+    function itemsResutSetToHtmlText($itemsResutSet)
+    {
+        
+    }
+
+    function get_category_items($categoryId)
+    {
+        $sqlCall = 'Call getItems(' . $categoryId . ')';
+        echo '<h1>A' . $sqlCall & '</h1>';
+        $itemsResutSet = $this->db->query($sqlCall);
+
+        $output = "";
+        foreach ($itemsResutSet->result_array() as $item) {
+            $output .= $item['item_title'];
+            $output .= " ";
+            $output .= $item['item_desc'];
+            $output .= "</br>";
+        }
+        return $output;
+    }
+
 }
-
 
 ?>
